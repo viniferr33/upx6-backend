37f562d79c61c8f71d52139250510068d06a13d0
Vinicius Ferreira Gomes <vini.ferr33@gmail.com>
2022-11-09 17:52:53 -0300

    added dinamic port

--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,5 @@
 import app from "./server";
 
-app.listen('8080', () => console.log("Server running on http://localhost:8080"));
+app.listen(process.env.PORT || 8080, () =>
+  console.log("Server running on http://localhost:8080")
+);
